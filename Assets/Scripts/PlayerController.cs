a08c4077c7054303712aacc50b9734e210b24176
AlonsoRial <alonsorivasalcobendas@gmail.com>
2025-01-26 00:44:13 +0100

    seguimiento de cámara + prueba colisión escaleras

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerController : MonoBehaviour
 {
-
+    PlayerInput playerInput;
     Rigidbody2D rigidbody2;
     [SerializeField] private float velocidadMovimiento;
 
+    public float velocidadCamara;
 
     Vector2 vector2 = Vector2.zero;
 
@@ -16,6 +18,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rigidbody2 = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
@@ -23,6 +26,9 @@ public class PlayerController : MonoBehaviour
     {
        // vector2.x = Input.GetAxisRaw("Horizontal");
         rigidbody2.transform.position += new Vector3(vector2.x,0,0) * Time.deltaTime * velocidadMovimiento;
+
+        playerInput.camera.transform.position = new Vector3(rigidbody2.position.x, rigidbody2.position.y, -10);
+
     }
 
 
