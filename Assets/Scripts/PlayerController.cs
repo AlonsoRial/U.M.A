aeb67bb279a855308c4fe04215de42a561580a66
AlonsoRial <alonsorivasalcobendas@gmail.com>
2025-01-25 23:01:42 +0100

    wasd con el nuevo input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerController : MonoBehaviour
 {
@@ -20,9 +21,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        vector2.x = Input.GetAxisRaw("Horizontal");
+       // vector2.x = Input.GetAxisRaw("Horizontal");
         rigidbody2.transform.position += new Vector3(vector2.x,0,0) * Time.deltaTime * velocidadMovimiento;
     }
+
+
+    public void Mover(CallbackContext callbackContext)
+    {
+
+        vector2 = callbackContext.ReadValue<Vector2>();
+
+        if (vector2.y != 0) 
+        {
+            Debug.Log(vector2);
+        }
+
+    }
+
 
 
 
