f306346cb00430231316c4a0d1a90f7213016548
AlonsoRial <alonsorivasalcobendas@gmail.com>
2025-02-04 22:10:54 +0100

    Comentarios y pequeño arreglo del flip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using static UnityEngine.InputSystem.InputAction;
 
+
+
+//Hola Eric, te comento el codigo para que lo entiendas un poco mejor :)
 public class PlayerController : MonoBehaviour
 {
+    // El PlayerInput maneja el nuevo sistema de inputs y la camara, si queremos que el jugador pueda cambiar los controles, se debe de hacer con esto, en teoria...
     PlayerInput playerInput;
     Rigidbody2D rigidbody2;
     Vector2 vector2 = Vector2.zero;
@@ -16,9 +20,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float velocidadSalto;
 
 
-
     [Header("SWITCH")]
     [SerializeField] private bool estadoSwitch = false;
+
 
     [Header("SALTO")]
     [SerializeField] private Transform groundTransform;
@@ -28,16 +32,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int cantidadSaltos;
     [SerializeField] private int saltosRestantes;
 
+
     [Header("SALTO MURO")]
     [SerializeField] private Transform muroTransform;
     [SerializeField] private Vector2 dimensionHitBoxSaltoMuro;
 
-    [Header("Dash")]
+
+    [Header("DASH")]
     [SerializeField] private float velocidadDash;
     [SerializeField] private bool botonDash;
     [SerializeField] private int cantidadDash;
     [SerializeField] private int dashRestantes;
-
     [SerializeField] private float tiempoDash;
 
     // Start is called before the first frame update
@@ -50,36 +55,34 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Metodo para voltear al personaje al moverse
         Flip();
-        // vector2.x = Input.GetAxisRaw("Horizontal");
 
-        // el transform positition es de tontos
-        // rigidbody2.transform.position += new Vector3(vector2.x,0,0) * Time.deltaTime * velocidadMovimiento;
-
-
+        //Su finción es que la camara siga al jugadador
+        //No se si podras, pero cuando te pongas con la camará, prueba con el playerInput.camara
+        //Supuestamete tiene las mismas funciones que solo Camara
         playerInput.camera.transform.position = new Vector3(rigidbody2.position.x, rigidbody2.position.y, -10);
 
 
-
+        //Si la hitbox del salto toca el suelo (concretamente la capa "Water", me dio flojera crear una capa nueva), se recarga los saltos y los dashes
         if (EstaEnTierra()) {
             saltosRestantes = cantidadSaltos;
             dashRestantes = cantidadDash;
         }
 
-
-
     }
 
-
+    //Update para fisicas
     private void FixedUpdate()
     {
-
+        //Si el boton del salto esta activado
         if (botonSalto) 
         {
+            //Si esta tocando la capa de tierra el colisionador de salto, llama el metodo de salto
             if (EstaEnTierra()) 
             {
                 Salto();
-            }
+            }//Para hacer el doble salto, si no esta el tierra, el boton de salto vuelve a estar activo (por que el frame del primero es distinto) y tiene saltos restantes, salta otra vez
             else if (!EstaEnTierra() && botonSalto && saltosRestantes>1) 
             {
                 Salto();
@@ -88,17 +91,19 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-
+        //Si el botón dash no esta activo, el personaje se mueve normal
         if (!botonDash)
         {
             rigidbody2.velocity = new Vector2(vector2.x * velocidadMovimiento, rigidbody2.velocity.y);
         }
+        //Si esta activo y le quedan dashes, esté se movera a la velocidad del dash
         else if (botonDash && dashRestantes > 0)
         {
-            //rigidbody2.velocity = vector2 * velocidadDash * velocidadMovimiento;
+            
             rigidbody2.velocity = new Vector2(vector2.x * velocidadDash, vector2.y * velocidadDash);
             dashRestantes--;
             
+            //Llama al "hilo"
             StartCoroutine("Espera");
             
         }
@@ -106,24 +111,23 @@ public class PlayerController : MonoBehaviour
         botonSalto = false;
     }
 
-
+    //Metodo para voltear el personaje
     public void Flip() 
     {
 
-
         if (vector2.x > 0)
-        {
-          
+        { 
             rigidbody2.transform.localRotation = Quaternion.Euler(0,0,0);
         }
         else if (vector2.x < 0) 
         {
-            
             rigidbody2.transform.localRotation = Quaternion.Euler(0, 180, 0);
         }
         
     }
 
+    //Usado para limitar la duración del dash
+    //No me ha quedado muy claro, pero es como un hilo, espera el tiempo indicado y luego devuelve false al boton de dash
     public IEnumerator Espera() 
     {
         
@@ -139,24 +143,33 @@ public class PlayerController : MonoBehaviour
         botonSalto = false;
     }
 
+    //Metodo booleano que devuelve si la colision del salto esta tocando la capa del suelo
     public bool EstaEnTierra() 
     {
         return Physics2D.OverlapBox(groundTransform.position, dimensionHitBoxSalto, 0,groundLayer);
     }
 
+
+    //Todos estos metodos son del apartado de los controles
+    //Si pone CallbackContext, son los controles
+    //el callbackContext es el valor del input
+    
+    //Existe una forma de hacerlo sin crear tantos metodos, simplemente creando y llamando a un objeto para que pasé su valor, pero a mi me gustá más asi, da más control
+    //Cambiar de una forma a otra no seria un gran ploblema, igual con el antiguo metodo
+
     public void Mover(CallbackContext callbackContext)
     {
-
+        //Está sería la tradución con el antiguo metodo
+        //vector2 = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         vector2 = callbackContext.ReadValue<Vector2>();
-
-        //Debug.Log(vector2);
-
     }
 
     public void CambioSwitch(CallbackContext callbackContext) 
     {
+        //bool boton = Input.GetKey(KeyCode.K);
         bool estado = callbackContext.ReadValueAsButton();
         
+        //if para que se mantenga el estado booleano, ya que el el input solo reaciona mientras esté pulsado 
         if (!estado) 
         {
             estadoSwitch = !estadoSwitch;
@@ -167,15 +180,18 @@ public class PlayerController : MonoBehaviour
 
     public void Jump(CallbackContext callbackContext)
     {
+        //bool boton = Input.GetKey(KeyCode.W);
         botonSalto = callbackContext.ReadValueAsButton();
     }
 
 
     public void Dash(CallbackContext callbackContext)
     {
+        //bool boton = Input.GetKey(KeyCode.J);
         botonDash = callbackContext.ReadValueAsButton();
     }
 
+        //Se encarga de dibujar colisiones, de momento solo el salto
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
