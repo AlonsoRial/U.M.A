2707fc8c1827352b9779797385e702e286de22da
AlonsoRial <alonsorivasalcobendas@gmail.com>
2025-02-08 13:14:50 +0100

    Escena Switch Implementada, aunque puede mejorar
    El código siento que no es el mejor a la hora de cambiar de color.
    
    En vez de tratar a los muros de forma individual, lo correcto seria tratarlo como conjunto en el escenario

---
+++ Assets/Prefabs/SwtichScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwtichScript : MonoBehaviour
+{
+
+    public PlayerController jugador;
+
+    BoxCollider2D boxCollider;
+    SpriteRenderer spriteRenderer;
+
+    public bool estado;
+    //tonto quien lo lea XD
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();   
+        spriteRenderer = GetComponent<SpriteRenderer>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+        estado = jugador.estadoSwitch;
+
+        if (this.CompareTag("ROJO"))
+        {
+            estado = !estado;
+        }
+
+        boxCollider.enabled = estado;
+
+        
+        if (estado)
+        {
+
+            if (this.CompareTag("ROJO"))
+            {
+                spriteRenderer.color = new Color(255, 0, 0, 255);
+            }
+
+            if (this.CompareTag("VERDE"))
+            {
+                spriteRenderer.color = new Color(0, 255, 0, 255);
+            }
+
+        }
+        else 
+        {
+            spriteRenderer.color = new Color(0, 0, 255, 255);
+
+        }
+       
+    }
+
+    
+}
