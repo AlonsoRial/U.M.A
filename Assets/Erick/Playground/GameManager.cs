79a0eff9d4c229d25276d968a255dc9403b19683
KinguRar <elemoinenat@gmail.com>
2025-03-25 11:44:54 +0100

    Animator

---
+++ Assets/Erick/Playground/GameManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("TutorialSample", LoadSceneMode.Single);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single); 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
+public class TempButton : MonoBehaviour
+{
+    public Button startButton;
+    public Button menuButton;
+    public Button quitButton;
+
+    void Start()
+    {
+        if (startButton != null)
+            startButton.onClick.AddListener(() => GameManager.Instance.LoadGame());
+
+        if (menuButton != null)
+            menuButton.onClick.AddListener(() => GameManager.Instance.LoadMainMenu());
+
+        if (quitButton != null)
+            quitButton.onClick.AddListener(() => GameManager.Instance.QuitGame());
+    }
+}
