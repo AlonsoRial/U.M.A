7f0d62dfddb2529bcf6ac8e595249bdb2889965d
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2025-04-03 11:01:57 +0200

    Hazards con Tiempo más personalizable

---
+++ Assets/Prefabs/Hazards & CheckPoint/Hazards con Tiempo/HazardsTiempo.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HazardPinchos : MonoBehaviour
+{
+
+    [SerializeField] private float tiempoDesactivado;
+    [SerializeField] private float tiempoActivado;
+    [SerializeField]  private float tiempoDurmiendo;
+    private SpriteRenderer sprite;
+    private BoxCollider2D boxcollider2D;
+    private float tiempo;
+  
+    void Awake()
+    {
+       
+        sprite = GetComponent<SpriteRenderer>();
+        boxcollider2D = GetComponent<BoxCollider2D>();
+       
+        StartCoroutine(PinchosTime());
+    }
+
+
+    IEnumerator PinchosTime()
+    {
+        yield return new WaitForSeconds(tiempoDurmiendo);
+        Debug.Log("Despierto");
+        while (true)
+        {
+
+            tiempo = boxcollider2D.enabled == true ? tiempoActivado : tiempoDesactivado;
+            Debug.Log(tiempo);
+            yield return new WaitForSeconds(tiempo);
+            sprite.enabled = !sprite.enabled;
+            boxcollider2D.enabled = !boxcollider2D.enabled;
+          
+        }
+    }
+
+}
