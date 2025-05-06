1e733fc5a768249b50d3bd33c6c5ad56cd79e9ce
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-05-06 11:11:57 +0200

    cambio Final
    Adrian manco

--- Assets/Scripts/Ascensor_Scrip2.cs
+++ Assets/Scripts/Ascensor_Scrip2.cs
@@ -4,7 +4,24 @@ using UnityEngine.SceneManagement;
 public class Ascensor_Scrip2 : MonoBehaviour
 {
     [SerializeField] private string nombreEscena;
-    
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            collision.gameObject.SetActive(false);
+            animator.SetTrigger("Funciona");
+
+        }
+    }
+
     void Cambio() 
     {
         if (!string.IsNullOrEmpty(nombreEscena))
